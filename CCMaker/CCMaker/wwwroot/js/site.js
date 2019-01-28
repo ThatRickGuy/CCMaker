8a35e871b47207ceca81f8b5a9420527cfd712e4
rwayyy <Richard.Way@deancare.com>
2019-01-28 14:49:03 -0600

    added save functionality

--- CCMaker/CCMaker/wwwroot/js/site.js
+++ CCMaker/CCMaker/wwwroot/js/site.js
@@ -1,5 +1,8 @@
-﻿var drag = d3.behavior.drag()
+﻿var selected;
+
+var drag = d3.behavior.drag()
     .on("drag", function (d, i) {
+        selected = d;
         d.x += d3.event.dx
         d.y += d3.event.dy
         d3.select(this).attr("transform", function (d, i) {
@@ -56,8 +59,8 @@ function model_factory(data) {
 
     var plate = d3.select("#svg")
         .append("svg:g")
-        .data([{ "x": 0, "y": 0 }])
-        .attr("transform", "translate(" + 0 + "," + 0 + ")")
+        .data([{ "x": 50, "y": 50 }])
+        .attr("transform", "translate(" + 50 + "," + 50 + ")")
         .attr("class", data.Name)
         .call(drag);
     //
@@ -67,17 +70,37 @@ function model_factory(data) {
     var lines = data.Boxes.split('\n');
     var LongestLine = 0
     lines.forEach(function (current_value) {
-        if (current_value.length > LongestLine)
-            LongestLine = current_value.length;
+        var linewidth = 0;
+        for (var i = 0; i < current_value.length; i++) {
+            if (current_value[i] == "-" || current_value[i] == "\n" || current_value[i] == "^") {
+                //do nothing, these do not add to width
+            }
+            else if (current_value[i] == ">") {
+                linewidth += .55;
+            }
+            else {
+                linewidth += 1;
+            }
+        }
+        if (linewidth > LongestLine)
+            LongestLine = linewidth;
     });
 
+    var LinesHeight = 0;
+    for (var i = 0; i < lines.length; i++) {
+        if (lines[i].indexOf("-") >= 0 && lines[i].indexOf("^") == -1) {
+            LinesHeight += .55;
+        }
+        LinesHeight++;
+
+    }
 
     console.log("create border");
     var border = plate.append("svg:rect")
         .attr("x", 2)
         .attr("y", 9)
         .attr("width", LongestLine * 23 + 3)
-        .attr("height", lines.length * 23 + 7)
+        .attr("height", LinesHeight * 23 + 7)
         .attr("rx", 3)
         .attr("ry", 3)
         .attr("stroke", "black")
@@ -243,3 +266,112 @@ function model_factory(data) {
 
 
 }
+
+
+
+
+
+
+
+var exportSVG = function (svg) {
+    document.getElementById('SaveLink').innerHTML = "";
+    // first create a clone of our svg node so we don't mess the original one
+    var clone = svg.cloneNode(true);
+    // parse the styles
+    parseStyles(clone);
+
+    // create a doctype
+    var svgDocType = document.implementation.createDocumentType('svg', "-//W3C//DTD SVG 1.1//EN", "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd");
+    // a fresh svg document
+    var svgDoc = document.implementation.createDocument('http://www.w3.org/2000/svg', 'svg', svgDocType);
+    // replace the documentElement with our clone 
+    svgDoc.replaceChild(clone, svgDoc.documentElement);
+    // get the data
+    var svgData = (new XMLSerializer()).serializeToString(svgDoc);
+
+    // now you've got your svg data, the following will depend on how you want to download it
+    // e.g yo could make a Blob of it for FileSaver.js
+    /*
+    var blob = new Blob([svgData.replace(/></g, '>\n\r<')]);
+    saveAs(blob, 'myAwesomeSVG.svg');
+    */
+    // here I'll just make a simple a with download attribute
+
+    var a = document.createElement('a');
+    a.href = 'data:image/svg+xml; charset=utf8, ' + encodeURIComponent(svgData.replace(/></g, '>\n\r<'));
+    a.download = 'myAwesomeSVG.svg';
+    a.innerHTML = 'Download SVG';
+    document.getElementById('SaveLink').appendChild(a);
+    var s = document.createElement('span');
+    var d = new Date();
+    s.innerHTML = ' Rendered at ' + d.getHours() + ":" + pad(d.getMinutes()) + ":" + pad(d.getSeconds());
+    document.getElementById('SaveLink').appendChild(s);
+
+};
+
+function pad(num) {
+    var s = "00" + num;
+    return s.substr(s.length - 2);
+}
+
+var parseStyles = function (svg) {
+    var styleSheets = [];
+    var i;
+    // get the stylesheets of the document (ownerDocument in case svg is in <iframe> or <object>)
+    var docStyles = svg.ownerDocument.styleSheets;
+
+    // transform the live StyleSheetList to an array to avoid endless loop
+    for (i = 0; i < docStyles.length; i++) {
+        styleSheets.push(docStyles[i]);
+    }
+
+    if (!styleSheets.length) {
+        return;
+    }
+
+    var defs = svg.querySelector('defs') || document.createElementNS('http://www.w3.org/2000/svg', 'defs');
+    if (!defs.parentNode) {
+        svg.insertBefore(defs, svg.firstElementChild);
+    }
+    svg.matches = svg.matches || svg.webkitMatchesSelector || svg.mozMatchesSelector || svg.msMatchesSelector || svg.oMatchesSelector;
+
+
+    // iterate through all document's stylesheets
+    for (i = 0; i < styleSheets.length; i++) {
+        var currentStyle = styleSheets[i]
+
+        var rules;
+        try {
+            rules = currentStyle.cssRules;
+        } catch (e) {
+            continue;
+        }
+        // create a new style element
+        var style = document.createElement('style');
+        // some stylesheets can't be accessed and will throw a security error
+        var l = rules && rules.length;
+        // iterate through each cssRules of this stylesheet
+        for (var j = 0; j < l; j++) {
+            // get the selector of this cssRules
+            var selector = rules[j].selectorText;
+            // probably an external stylesheet we can't access
+            if (!selector) {
+                continue;
+            }
+
+            // is it our svg node or one of its children ?
+            if ((svg.matches && svg.matches(selector)) || svg.querySelector(selector)) {
+
+                var cssText = rules[j].cssText;
+                // append it to our <style> node
+                style.innerHTML += cssText + '\n';
+            }
+        }
+        // if we got some rules
+        if (style.innerHTML) {
+            // append the style node to the clone's defs
+            defs.appendChild(style);
+        }
+    }
+
+};
